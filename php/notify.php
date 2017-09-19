e2fa01af36f65ed323d2f330d1127db778ccd845
abc <abc@example.com>
2017-09-19 23:09:42 +0530

    change index file

--- php/notify.php
+++ php/notify.php
@@ -16,7 +16,7 @@ $headers = 'From: '  . '<' . $_POST["email"] . '>' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
   //
-  mail( "vijayanpp02@gmail.com",$_POST["email"], $headers );
+  mail( "ctpathirana@gmail.com",$_POST["email"], $headers );
  
   //      ^
   //  Replace with your email 
